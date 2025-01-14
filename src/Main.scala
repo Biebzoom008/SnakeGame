88b96539d7f570ddcd19d8bce200b4150914cf84
Biebzoom008 <theodelalayx@gmail.com>
2025-01-14 13:37:45 +0100

    Image adding fixed!!

--- src/Main.scala
+++ src/Main.scala
@@ -17,7 +17,9 @@ object Main {
 
     grid.drawGrid()
 
-
+    grid.display.drawForeground()
+    grid.display.drawTransformedPicture(grid.cornerSize*30, grid.headerSize*30,0,0.05,"/res/strawberry.png")
+    grid.display.drawTransformedPicture(grid.cornerSize*30*3, grid.headerSize*30*3,0,0.05,"/res/weirdStraw.jpg")
     grid.display.syncGameLogic(60)
     while(true){
       Thread.sleep(1000)
