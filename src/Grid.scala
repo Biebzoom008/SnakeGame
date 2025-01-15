805e5d6eb09dab835b342caf3b2836c94e742805
Biebzoom008 <theodelalayx@gmail.com>
2025-01-15 13:25:34 +0100

    Deathscreen 80% complete

--- src/Grid.scala
+++ src/Grid.scala
@@ -30,22 +30,25 @@ class Grid (){
       }
     }
 
-    var foodTimer : Int = 0
 
 
     def createFood() : Unit = {
       var randomX : Int = (Math.random()*15).toInt
       var randomY : Int = (Math.random()*15).toInt
-      foodTimer += 1
+
       if (((foodGrid(snake.position(0)-1)(snake.position(1)-4) == 1) || (foodGrid(snake.position(0)-1)(snake.position(1)-4) == 2)) && gridElement(randomX)(randomY) == 0 && foodGrid(randomX)(randomY) == 0){
-        foodTimer = 0
         display.drawTransformedPicture(cornerSize * 30 + randomX*cellSize + 15, headerSize * 30 + randomY*cellSize + 45, 0, 0.05, "/res/strawberry.png")
         foodGrid(randomX)(randomY) = 1
-      } else if (foodGrid(randomX)(randomY)==1){
+      }
+    }
+
+    def createFood2() : Unit = {
+      var randomX : Int = (Math.random()*15).toInt
+      var randomY : Int = (Math.random()*15).toInt
+      if (((foodGrid(snake.position(0)-1)(snake.position(1)-4) == 1) || (foodGrid(snake.position(0)-1)(snake.position(1)-4) == 2)) && gridElement(randomX)(randomY) == 0 && foodGrid(randomX)(randomY) == 0){
         drawEmpty(randomX,randomY)
         display.drawTransformedPicture(cornerSize * 30 + randomX*cellSize + 15, headerSize * 30 + randomY*cellSize + 45, 0, 0.175, "/res/Goldberry_ingame.png")
         foodGrid(randomX)(randomY) = 2
-
       }
     }
 
@@ -75,6 +78,14 @@ class Grid (){
   val backgroundGreen : Color = new Color(100, 136, 64)
   val headerGreen : Color = new Color(85, 115, 54)
 
+  val lightGreenT : Color = new Color (178, 215, 95,125)
+  val greenT : Color = new Color (157, 201, 82,125)
+  val backgroundGreenT : Color = new Color(100, 136, 64, 125)
+  val headerGreenT : Color = new Color(85, 115, 54, 125)
+
+
+
+
 
   //Creation of the display here to make everything cleaner
   //Creation of the snake and food objects
@@ -88,25 +99,25 @@ class Grid (){
   display.setKeyManager(new KeyAdapter() { // Will be called when a key has been pressed
     override def keyPressed(e: KeyEvent): Unit = {
       if (e.getKeyChar == 'a') println("The key 'A' was pressed")
-      if (e.getKeyCode == KeyEvent.VK_RIGHT) {
+      if (e.getKeyCode == KeyEvent.VK_RIGHT && snake.left == false) {
         snake.left = false
         snake.down = false
         snake.up = false
         snake.right = true
       }
-      if (e.getKeyCode == KeyEvent.VK_LEFT) {
+      if (e.getKeyCode == KeyEvent.VK_LEFT && snake.right == false) {
         snake.down = false
         snake.up = false
         snake.right = false
         snake.left = true
       }
-      if (e.getKeyCode == KeyEvent.VK_UP) {
+      if (e.getKeyCode == KeyEvent.VK_UP && snake.down == false) {
         snake.left = false
         snake.down = false
         snake.right = false
         snake.up = true
       }
-      if (e.getKeyCode == KeyEvent.VK_DOWN) {
+      if (e.getKeyCode == KeyEvent.VK_DOWN && snake.up == false) {
         snake.left = false
         snake.up = false
         snake.right = false
@@ -114,7 +125,6 @@ class Grid (){
       }
     }
   })
-
 
 
 
@@ -133,6 +143,7 @@ class Grid (){
     }
   }
 
+
   def drawBackGround(c: Color): Unit = {
     for (x <- 0 until width) {
       for (y <- 0 until height) {
@@ -148,20 +159,22 @@ class Grid (){
         display.setPixel(x, y, c)
       }
     }
+    display.drawTransformedPicture(width/4, headerSize*cellSize/2, 0, 0.05, "/res/strawberry.png")
+    scoreDisplay()
   }
 
 
   //Draws the grid unless there is the snake (early iteration of the snake head)
-  def drawGrid () : Unit = {
+  def drawGrid (c1 : Color, c2 : Color) : Unit = {
     {
       display.drawBackground()
       for (x <- cornerSize until numCellsX - cornerSize) {
         for (y <- headerSize + cornerSize until numCellsY - cornerSize) {
           // Alterne les couleurs en fonction de la position
           if (((x + y) % 2 == 0) )   {
-            drawCell(x, y, lightGreen) // vert clair
+            drawCell(x, y, c1) // vert clair
           } else if(!((x + y) % 2 == 0) ){
-            drawCell(x, y, green) // Vert foncé
+            drawCell(x, y, c2) // Vert foncé
           }
         }
       }
@@ -193,11 +206,11 @@ class Grid (){
   def drawGame() : Unit = {
     for(x <- gridElement.indices){
       for (y <- gridElement(x).indices){
-        if (gridElement(x)(y)>1){
+        if (gridElement(x)(y)>0){
           drawCell(x+cornerSize,y+headerSize+1,blue)    //draws the snake in the cells where the value is higher than 0
         }
         if (gridElement(x)(y)>0) {      //>0 so that no cell value ever goes under 0
-          gridElement(x)(y) -= 1
+          gridElement(x)(y) -= 1        //That's why it's snake.size + 1, because we do -1 right away
         }
         drawEmpty(x,y)
       }
@@ -215,10 +228,17 @@ class Grid (){
 
   def scoreDisplay () : Unit = {
     display.drawString(width/6,height/8, s"This is your score : ${snake.score}","Arial",Font.PLAIN,20, blue)
+    display.drawTransformedPicture(width/4, headerSize*cellSize/2, 0, 0.05, "/res/strawberry.png")
   }
 
   def deathScreen() : Unit = {
-    display.clear()
+
+    display.drawBackground()
+    drawBackGround(backgroundGreenT)
+    drawGrid(greenT, lightGreenT)
+    drawHeader(headerGreenT)
+
+    display.drawForeground()
     display.drawString(width/4,height/2, "YOU DIEDDDDD", blue, 20)
     scoreDisplay()
   }
--- src/Main.scala
+++ src/Main.scala
@@ -4,7 +4,7 @@ object Main {
   def main(args: Array[String]): Unit = {
 
 
-
+    println("hi")
     //Creation of the grid and the snake
     val grid : Grid = new Grid ()
 
@@ -12,31 +12,37 @@ object Main {
     //Drawing the background and header
     grid.display.drawBackground()
     grid.drawBackGround(grid.backgroundGreen)
-    grid.drawHeader(grid.headerGreen)
-    grid.drawGrid()
+    grid.drawGrid(grid.green, grid.lightGreen)
 
-    grid.display.drawForeground()
 
     var randomX : Int = (Math.random()*15).toInt
     var randomY : Int = ((Math.random()*15).toInt)
     grid.display.syncGameLogic(30)
+    grid.drawEmpty(randomX,randomY)
     grid.food.foodGrid(randomX)(randomY)= 1
+    grid.display.drawForeground()
     grid.display.drawTransformedPicture(grid.cornerSize * 30 + randomX*grid.cellSize + 15, grid.headerSize * 30 + randomY*grid.cellSize + 45, 0, 0.05, "/res/strawberry.png")
+    grid.display.drawForeground()
+    grid.drawHeader(grid.headerGreen)
+
+
     try{while(!grid.tailDeath){
       grid.move()
       grid.drawGame()
+      println(grid.snake.position(1))
+      var random : Int = (Math.random()*5).toInt
 
-      if((Math.random()+1).toInt ==1){
-      grid.food.createFood()
+      if(random > 1){
+        grid.food.createFood()
       } else {
-        grid.food.createFood()
-        grid.food.createFood()
+        grid.food.createFood2()
+
       }
 
       grid.food.eat()
-      grid.drawHeader(grid.headerGreen)
-      grid.scoreDisplay()
-      Thread.sleep(250)
+
+
+      Thread.sleep(300)
       }
     } catch {
       case e : ArrayIndexOutOfBoundsException => grid.deathScreen()
